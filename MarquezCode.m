3997bb34cee7e1fa1fdc6a2354793c4276f409f9
Princess Mae Marquez <161012750+princessmaemm@users.noreply.github.com>
2024-02-27 07:53:55 +0800

    Add files via upload

---
+++ MarquezCode.m
@@ -0,0 +1,16 @@
+% Clear
+clear
+clc
+close all
+
+syms t s
+
+%% Laplace Transform
+f3 = (t + 1) * (t + 2);
+F3 = laplace(f3);
+pretty(F3)
+
+%% Inverse Laplace 
+F4 = (8 - 3*s + s^2) / s^3;
+f4 = ilaplace(F4);
+pretty(f4)
